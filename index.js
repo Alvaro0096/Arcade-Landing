b6321d00c023d83a7bf38ddc0141bd86735e5e5c
AlvaroJorge <alv.jor96@gmail.com>
2022-04-30 22:33:36 -0300

    Add blur

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 // AOS INIT ANIMATION
 AOS.init();
+
+//Navbar Scroll Hide
+let prevScrollpos = window.pageYOffset;
+window.onscroll = () => {
+    let navbar_scroll = document.querySelector('.nav-header');
+    let currentScrollPos = window.pageYOffset;
+    if(prevScrollpos > currentScrollPos){
+        navbar_scroll.style.top = '0';
+    } else {
+        navbar_scroll.style.top = '-400px';
+    }
+    prevScrollpos = currentScrollPos;
+}
